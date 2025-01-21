c0639808e918e6569342e11e153af570000fd5e6
emmunozce <emmanuel.munoz@softtek.com>
2025-01-20 18:22:40 -0600

    Funcionalidad arreglada, bug eliminado (#15)

--- main.py
+++ main.py
@@ -2,8 +2,12 @@ def main():
     print("Hello, World!")
     bug_fix()
 
+def bug_resolver():
+    print("Bug resolved")
+
 def bug_fix():
     print("Bug fix")
+    bug_resolver()
 
 if __name__ == '__main__':
     main()
